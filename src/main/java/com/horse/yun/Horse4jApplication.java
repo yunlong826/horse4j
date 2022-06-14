5b04f23ef9b75af709c75d47597a5d41382e7764
tom_glb <goodmorning_glb@atguigu.com>
2022-06-14 13:53:43 +0800

    Initial commit

---
+++ src/main/java/com/horse/yun/Horse4jApplication.java
@@ -0,0 +1,13 @@
+package com.horse.yun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Horse4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Horse4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/horse/yun/Horse4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horse.yun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Horse4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
